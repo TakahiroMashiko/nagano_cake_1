e997bacafc596939646e01bc4dab3b51a02d9f06
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-06 07:42:34 +0000

    [Add] Make a admin/orders_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'orders/index'
+    get 'orders/update'
+  end
+  namespace :admin do
     get 'genres/index'
     get 'genres/create'
     get 'genres/edit'
