26fd68228a8222f39b80c9df918a90be615bb79e
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-07 09:46:26 +0000

    [Update] Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  # Change login path
+  def after_sign_in_path_for(resource)
+    if customer_signed_in?
+      customers_path(resource)
+    else
+      admin_top_path
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
