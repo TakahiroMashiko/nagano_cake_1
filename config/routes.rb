eba2207680eb8a3dfa047eb71da271baa3cc396d
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-07 01:38:39 +0000

    [Add] Routing home

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,29 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'order_details/update'
-  end
-  namespace :admin do
-    get 'orders/index'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  devise_for :customers
+
+  # admin
   devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resource :home, :except => [:index, :create, :new, :edit, :show, :update, :destroy]
+    get  'top'=>'homes#top'
+    # get 'order_details/update'
+    # get 'orders/index'
+    # get 'orders/update'
+    # get 'genres/index'
+    # get 'genres/create'
+    # get 'genres/edit'
+    # get 'genres/update'
+    # get 'items/index'
+    # get 'items/new'
+    # get 'items/create'
+    # get 'items/show'
+    # get 'items/edit'
+    # get 'items/update'
+    # get 'homes/top'
+    # get 'customers/index'
+    # get 'customers/show'
+    # get 'customers/edit'
+    # get 'customers/update'
+  end
+  # customer
+  devise_for :customers
 end
