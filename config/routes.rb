5d102e7c8f3da6644a93ab59427716b3ce59d190
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-11 11:59:26 +0000

    [Update] Add resources :orders to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   devise_for :admins
   namespace :admin do
     resource :home, :except => [:index, :create, :new, :edit, :show, :update, :destroy]
-    get  'top'=>'homes#top'
-    resources :customers, only: [:index, :show, :edit, :update]
-    resources :items, only: [:index, :new, :create, :show, :edit, :update,]
+      get  'top'=>'homes#top'
+      resources :customers, only: [:index, :show, :edit, :update]
+      resources :items, only: [:index, :new, :create, :show, :edit, :update,]
+      resources :orders, only: [:index, :show, :update]
   end
 
   # customer
