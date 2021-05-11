9bf86322b0002e3e9489179061aaa1af73dd8213
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-11 11:38:27 +0000

    [Update] Add resources :customers to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,26 +5,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resource :home, :except => [:index, :create, :new, :edit, :show, :update, :destroy]
     get  'top'=>'homes#top'
-    ## コントローラー作成時に併せて生成されたルーティング、最後に削除する
-    # get 'order_details/update'
-    # get 'orders/index'
-    # get 'orders/update'
-    # get 'genres/index'
-    # get 'genres/create'
-    # get 'genres/edit'
-    # get 'genres/update'
-    # get 'items/index'
-    # get 'items/new'
-    # get 'items/create'
-    # get 'items/show'
-    # get 'items/edit'
-    # get 'items/update'
-    # get 'homes/top'
-    # get 'customers/index'
-    # get 'customers/show'
-    # get 'customers/edit'
-    # get 'customers/update'
+    resources :customers, only: [:index, :show, :edit, :update]
   end
+
   # customer
   devise_for :customers
 end
