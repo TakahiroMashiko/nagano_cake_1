c74f27aac2c32cf096e2d9c341f8706277b5422b
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-14 15:12:42 +0000

    [Update] Add strong parameters to admin/customers_controller.rb

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -20,4 +20,10 @@ class Admin::CustomersController < ApplicationController
 			render "edit"
 		end
 	end
+
+  # Strong parameters
+	private
+	def customer_params
+	  params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_active)
+	end
 end
