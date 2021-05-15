abdf7436698239ebb359cade2f5d0b51b9daaaaf
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-15 07:10:10 +0000

    [Update] Add def update a flash[:success] to admin/order_details_controller.rb

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,6 +2,7 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     if @order_detail.update(order_detail_params)
+      flash[:success] = "製作ステータスを変更しました"
 
     end
   end
