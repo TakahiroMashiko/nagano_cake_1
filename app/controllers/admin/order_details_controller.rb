5605801495a1c1a2c16ed8e5a968f6776a9ae8d5
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-15 07:17:39 +0000

    [Update] Add params.require(:order_detail) to admin/order_details_controller.rb

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -12,6 +12,6 @@ class Admin::OrderDetailsController < ApplicationController
   # Strong parameters
   private
   def order_detail_params
-
+    params.require(:order_detail).permit(:production_status)
   end
 end
