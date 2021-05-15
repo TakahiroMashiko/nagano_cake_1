12ae4ce51d491b5a011b44290b23c0503a0f744d
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-15 07:08:39 +0000

    [Update] Add def update an if @order_detail.update to admin/order_details_controller.rb

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,5 +1,8 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
+    if @order_detail.update(order_detail_params)
+
+    end
   end
 end
