0b488e4adbb47fd8fa7d85765aeb09bd9da621f3
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-21 11:55:20 +0000

    [Update] Add @orders = Order.page(params[:page]).per(10) to homes_controller.rb

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,5 @@
 class Admin::HomesController < ApplicationController
-  # kaminari実装後に「.page(params[:page]).per(10)」を記載
   def top
-    @orders = Order.all
+    @orders = Order.page(params[:page]).per(10)
   end
 end
