9b47b55193b7beda332d71d61fdd53735dd5fee8
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-21 08:24:59 +0000

    [Add] Add def full_name_kana(customer) to application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
     customer.last_name + customer.first_name
   end
 
+  # Customer's full name kana
+	def full_name_kana(customer)
+		customer.last_name_kana + customer.first_name_kana
+	end
+
 end
