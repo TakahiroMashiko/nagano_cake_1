0b31c6bb26d2dbd5da279a3923292d0f116815d7
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-22 10:51:30 +0000

    [Update] Add def update @item = Item.find(params[:id]) to admin/items_controller.rb

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -26,8 +26,8 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
     if @item.update(item_params)
-      flash[:success] = "商品内容を変更しました"
       redirect_to admin_item_path(@item)
     else
       render :edit
