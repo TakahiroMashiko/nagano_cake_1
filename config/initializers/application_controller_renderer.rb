571387d829d1c39158e843fe6fc4d60573c30969
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-22 07:12:57 +0000

    [Update] Add secret key to config/initializers/application_controller_renderer.rb

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+# Refile.secret_key was not set が表示されたため以下を追記
+Refile.secret_key = 'f11ef3f5b8571be3213f45651869ed24965761f6b2e93c3e86d69abb7a9ef27da2710b036ec296e4e5e0166a2f8eab02fea2497a71bf1495a10c5ace52ac859f'
