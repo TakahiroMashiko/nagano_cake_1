1fbaef3fc6c8608abf3e38212dc4b807c2647cb1
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-23 02:25:22 +0000

    [Add] Add def full_address(key) to application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,11 @@ module ApplicationHelper
 		customer.last_name_kana + customer.first_name_kana
 	end
 
+	# Customer's full address
+	def full_address(key)
+	  "#{key.shipping_postal_code} #{key.shipping_address} #{key.shipping_name}"
+	end
+
   # Tax-included calculation
   def tax_price(price)
     (price * 1.1).floor
