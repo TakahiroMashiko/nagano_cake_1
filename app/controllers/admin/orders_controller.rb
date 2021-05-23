618c686e5c496505081a238b5b2fa2f40f04ee08
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-23 03:48:53 +0000

    [Update] Add before_action :authenticate_admin! to admin/orders_controller.rb

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,7 @@
 class Admin::OrdersController < ApplicationController
+
+  before_action :authenticate_admin!
+
   def index
     @order = Order.find(params[:id])
     @order_details = @order.order_details
