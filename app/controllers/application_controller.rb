eca0e7941a035b560a397dc228e4a11ccf76f255
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-25 11:03:40 +0000

    [Update] Add devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number]) to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # New registration save function
   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
   end
 end
