fa58adce95338636f24bca777e566bcba6391035
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-25 13:01:06 +0000

    [Add] Add has_many shipping_addresses,orders,cart_items to customers.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,8 +4,11 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :address, :telephone_number,
-    presence: true
+  has_many :shipping_addresses, dependent: :destroy
+  has_many :orders
+  has_many :cart_items, dependent: :destroy
+
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :address, :telephone_number, presence: true
   validates :postal_code, length: {is: 7}, numericality: { only_integer: true }
   validates :telephone_number, numericality: { only_integer: true }
   validates :last_name_kana, :first_name_kana,
