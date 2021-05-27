20ba9dbcd073bc017d7e615714a409e73b64ee85
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-27 13:38:44 +0000

    [Add] Add has_many,validates to item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :order_items, dependent: :destroy
+
   attachment :image
+
+  validates :introduction, length: {maximum: 200}
+  validates :price, numericality: { only_integer: true }
 end
