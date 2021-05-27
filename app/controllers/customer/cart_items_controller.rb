4c3e6efa290eff70198343e41b8639ef995b5bc0
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-27 16:43:30 +0000

    [Update & Fix] Add flash[:success],fix def cart_item_params to cart_items_controller.rb

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -9,32 +9,33 @@ class Customer::CartItemsController < ApplicationController
     @cart_item = CartItem.find(params[:id])
     @cart_items = CartItem.where(customer_id: current_customer.id).order("created_at DESC")
     if @cart_item.update(cart_item_params)
-      render :index
+      flash[:success] = "個数を変更しました"
+      redirect_to cart_items_path
     else
       flash[:danger] = "正しい個数を入力してください"
-      render :index
+      redirect_to cart_items_path
     end
   end
 
-  def delete
+  def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_items = CartItem.where(customer_id: current_customer.id).order("created_at DESC")
     @cart_item.destroy
       flash[:success] = "選択した商品を削除しました"
-      render :index
+      redirect_to cart_items_path
   end
 
   def all_delete
     @cart_items = current_customer.cart_items.all
     @cart_items.destroy_all
       flash[:success] = "カートが空になりました"
-      render :index
+      redirect_to cart_items_path
   end
 
   def create
     @cart_items = CartItem.find_by(customer_id: current_customer.id, item_id: params[:cart_item][:item_id])
     if @cart_items.present?
-      @cart_items.number += params[:cart_item][:number].to_i
+      @cart_items.amount += params[:cart_item][:amount].to_i
       @cart_items.save
         flash[:success] = "カートに追加しました"
         redirect_to cart_items_path
@@ -53,7 +54,7 @@ class Customer::CartItemsController < ApplicationController
 
   # Strong parameters
   private
-  def cart_item_param
+  def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
   end
 end
