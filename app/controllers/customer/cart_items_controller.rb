0bbcf689691602a0de23d5abd939ff2e500cad38
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-27 11:42:50 +0000

    [Add] Add @cart_item = CartItem.find(params[:id]) to cart_items_controller.rb

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -2,12 +2,15 @@ class Customer::CartItemsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
+    @cart_item = CartItem.where(customer_id: current_customer.id).order("created_at DESC")
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
   end
 
   def delete
+    @cart_item = CartItem.find(params[:id])
   end
 
   def all_delete
