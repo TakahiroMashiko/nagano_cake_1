b379bd8801c2f9699b20f905369d110f5bfd4525
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-27 16:44:39 +0000

    [Update] Add def price_including_tax,def subtotal to cart_item.rb

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,4 +4,12 @@ class CartItem < ApplicationRecord
 
   validates :customer_id, :item_id, :amount, presence: true
   validates :amount, numericality: { only_integer: true }
+
+  def price_including_tax
+    (item.price*1.10).round
+  end
+
+  def subtotal
+    (item.price*1.10*amount).round
+  end
 end
