59fb5d1d8c6c3e82c5428b805a38d8772cf345b6
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-28 11:39:56 +0000

    [Update] Add validates :customer_id, :address, :name, :shipping_cost, :total_payment, :payment_method, presence: true to order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
 
   enum payment_method: { クレジットカード: 0, 銀行振込: 1 }
+
+  validates :customer_id, :address, :name, :shipping_cost, :total_payment, :payment_method, presence: true
 end
