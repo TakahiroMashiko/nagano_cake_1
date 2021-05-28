b2445fb736709a6709a45e015c3a053cb288d3f7
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-28 11:40:55 +0000

    [Update] Add validates :postal_code, presence: true, length: {is: 7}, numericality: { only_integer: true } true to order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,5 @@ class Order < ApplicationRecord
   enum payment_method: { クレジットカード: 0, 銀行振込: 1 }
 
   validates :customer_id, :address, :name, :shipping_cost, :total_payment, :payment_method, presence: true
+  validates :postal_code, presence: true, length: {is: 7}, numericality: { only_integer: true }
 end
