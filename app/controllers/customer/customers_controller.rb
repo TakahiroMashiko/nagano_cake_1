752715b46e110b074722130789adcf11adbfbe73
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-28 13:00:47 +0000

    [Update] Add def customer_params to customer/customers_controller.rb

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -21,6 +21,9 @@ class Customer::CustomersController < ApplicationController
     @customer = current_customer
   end
 
+  # Strong parameters
   private
-
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
+  end
 end
