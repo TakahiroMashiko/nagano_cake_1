399287304179e68b225eb46c9714ce93bc416c2c
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-29 11:49:12 +0000

    [Update] Add belongs_to :customer,validates :postal_code,address,name to address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,6 @@
 class Address < ApplicationRecord
+  belongs_to :customer
+
+  validates :postal_code, presence: true, length: { is: 7 }, numericality: { only_integer: true }
+  validates :address, :name, presence: true
 end
