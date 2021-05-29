1fd9cca4ac2fafdd601166c83cf920a185f0626c
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-29 01:47:08 +0000

    [Fix] Fix def update,edit,params.require(:customer).permit to customer/customers_controller.rb

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -7,17 +7,16 @@ class Customer::CustomersController < ApplicationController
 
   def edit
     @customer = current_customer
-    #　Customer::Customers#editに飛ばすため、一旦コメントアウト
-    # if @customer.update(customer_params)
-    #   flash[:success] = "会員情報を更新しました"
-    #   redirect_to customers_path
-    # else
-    #   render 'edit'
-    # end
   end
 
   def update
     @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:success] = "会員情報を更新しました"
+      redirect_to customers_path
+    else
+      render 'edit'
+    end
   end
 
   def withdrawal
@@ -35,6 +34,6 @@ class Customer::CustomersController < ApplicationController
   # Strong parameters
   private
   def customer_params
-    params.permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
+   params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
   end
 end
