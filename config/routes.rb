e18a476cc477f6eba54a62949b4850fcc3a9b87f
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-29 11:40:09 +0000

    [Fix] Fix devise routing to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -34,9 +34,9 @@ Rails.application.routes.draw do
   devise_for :customers, skip: :all
   devise_scope :customer do
     get 'customers/sign_up' => 'devise/registrations#new', as: :new_customer_registration
-    post 'customers' => 'devise/registrations#create', as: :customer_registration
+    post 'customers/sign_up' => 'devise/registrations#create', as: :customer_registration
     get 'customers/sign_in' => 'devise/sessions#new', as: :new_customer_session
-    post 'customers' => 'devise/sessions#create', as: :customer_session
+    post 'customers/sign_in' => 'devise/sessions#create', as: :customer_session
     delete '/customers/sign_out' => 'devise/sessions#destroy', as: :destroy_customer_session
   end
 
