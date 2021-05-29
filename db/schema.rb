08a4c3ebd317a02579f6c5d4a12353325f1bc101
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-29 19:09:11 +0000

    [Update] DB migrate orders to db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_185556) do
+ActiveRecord::Schema.define(version: 2021_05_29_190731) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -91,7 +91,7 @@ ActiveRecord::Schema.define(version: 2021_05_29_185556) do
   create_table "orders", force: :cascade do |t|
     t.integer "cuttomer_id"
     t.string "postal_code"
-    t.string "shipping_address"
+    t.string "address"
     t.string "shipping_name"
     t.integer "shipping_cost"
     t.integer "payment_method"
