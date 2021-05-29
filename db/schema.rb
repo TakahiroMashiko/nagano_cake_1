2bcce09e93daad9ead349942954c101d0a683db2
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-29 10:19:47 +0000

    [Update] DB migrate addresses to db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_040845) do
+ActiveRecord::Schema.define(version: 2021_05_29_065248) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
-    t.string "addresses"
+    t.string "name"
     t.string "postal_code"
     t.string "address"
     t.datetime "created_at", null: false
