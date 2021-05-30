4f14ddb5efa605d583afece121ec51d315a3fc91
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-30 13:56:43 +0000

    [Update] Add comment out to order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
-  accepts_nested_attributes_for :order_details
+  # accepts_nested_attributes_for :order_details
 
   enum payment_method: { クレジットカード: 0, 銀行振込: 1 }
 
