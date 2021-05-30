feb3012346345c4e68d07451e279e233b2b9cc45
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-05-30 12:35:10 +0000

    [Update] Add has_many :order_details, dependent: :destroy to item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
-  has_many :order_items, dependent: :destroy
+  has_many :order_details, dependent: :destroy
 
   attachment :image
 
