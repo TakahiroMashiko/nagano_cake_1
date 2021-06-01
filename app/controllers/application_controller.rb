23c5d5a55d4c0af35b199c9edc8fca202e6ff32c
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-06-01 14:04:41 +0000

    [Fix] Fix def configure_permitted_parameters to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
   # Change login path
   def after_sign_in_path_for(resource)
     if customer_signed_in?
@@ -12,12 +11,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # New registration save function
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
-  end
-
   # Change loggout path
   def after_sign_out_path_for(resource)
     if resource == :customer
@@ -26,5 +19,12 @@ class ApplicationController < ActionController::Base
     else
       new_admin_session_path
     end
+  end
+
+  protected
+  # New registration save function
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
   end
 end
