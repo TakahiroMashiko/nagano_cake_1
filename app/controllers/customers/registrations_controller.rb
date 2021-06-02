cb83c540bc4c490dd7e9b4fc1b8edc900278de2f
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-06-02 12:14:50 +0000

    [Fix] Fix def after_sign_up_path_for(resource) to customers/registrations_controller.rb

--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -41,9 +41,9 @@ class Customers::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :last_name, :first_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_deleted])
-  end
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :last_name, :first_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_deleted])
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
@@ -56,7 +56,7 @@ class Customers::RegistrationsController < Devise::RegistrationsController
   end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    customers_path
+  end
 end
