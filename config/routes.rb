8055a70ca78f54ba6bbef31caf8e5b20f3eb688c
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-06-02 12:13:38 +0000

    [Fix] Fix devise_scope :customer do to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,11 @@ Rails.application.routes.draw do
 
   devise_for :customers, skip: :all
   devise_scope :customer do
-    get 'customers/sign_up' => 'devise/registrations#new', as: :new_customer_registration
-    post 'customers/sign_up' => 'devise/registrations#create', as: :customer_registration
-    get 'customers/sign_in' => 'devise/sessions#new', as: :new_customer_session
-    post 'customers/sign_in' => 'devise/sessions#create', as: :customer_session
-    delete '/customers/sign_out' => 'devise/sessions#destroy', as: :destroy_customer_session
+    get 'customers/sign_up' => 'customers/registrations#new', as: :new_customer_registration
+    post 'customers/sign_up' => 'customers/registrations#create', as: :customer_registration
+    get 'customers/sign_in' => 'customers/sessions#new', as: :new_customer_session
+    post 'customers/sign_in' => 'customers/sessions#create', as: :customer_session
+    delete '/customers/sign_out' => 'customers/sessions#destroy', as: :destroy_customer_session
   end
 
   scope module: :customer do
